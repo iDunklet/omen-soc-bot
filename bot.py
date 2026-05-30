92a15daed29fbfa8b1427ff0e37d62e684630e41
Hugo <dunkletodeus@gmail.com>
2026-05-31 00:11:50 +0200

    cambios botpy

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
-from config import DISCORD_TOKEN
+from discord import app_commands
+
+from config import DISCORD_TOKEN, GUILD_ID
 
 intents = discord.Intents.default()
-intents.message_content = True
 
 bot = commands.Bot(
     command_prefix="!",
@@ -13,5 +14,23 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
+    try:
+        guild = discord.Object(id=GUILD_ID)
+        synced = await bot.tree.sync(guild=guild)
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+
+
+@bot.tree.command(
+    name="ping",
+    description="Check bot status"
+)
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        "🏓 Pong!"
+    )
+
 
 bot.run(DISCORD_TOKEN)
